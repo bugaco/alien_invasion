585fd3f651f7b08ce8e317c5a70427b945124daf
zephyr <liyizhezhe@gmail.com>
2018-07-02 17:32:26 +0800

    现在，飞船可以发射子弹并射杀外星人了

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 
 def run_game():
@@ -18,18 +19,21 @@ def run_game():
     # 设置背景色
     bg_color = (230, 230, 230)
 
-    # 创建一艘飞船
+    # 创建一艘飞船、一个子弹group和一个飞船group
     ship = Ship(ai_settings, screen)
-
-    # 创建一个用于存储子弹的group
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
